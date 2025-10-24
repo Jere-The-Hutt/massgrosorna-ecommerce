e4a80bd440fe3ee14962e0fd4cbc5bbf13109762
Jere-The-Hutt <jere.huttunen@live.com>
2025-10-24 10:19:26 +0200

    Initial Django setup: added store app, templates, static files, and homepage working

---
+++ store/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def library(request):
+    return render(request, 'library.html')
+
+
+def success(request):
+    return render(request, 'success.html')
