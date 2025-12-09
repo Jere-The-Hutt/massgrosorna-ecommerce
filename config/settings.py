e681b028885fcefdb71ba92b15f1074600f5d244
Jere-The-Hutt <jere.huttunen@live.com>
2025-12-09 10:05:28 +0100

    Refactor Cloudinary configuration to use dictionary format in settings

--- config/settings.py
+++ config/settings.py
@@ -51,12 +51,12 @@ INSTALLED_APPS = [
     'cart',
 ]
 
-# Cloudinary config
-cloudinary.config(
-    cloud_name=config('CLOUDINARY_CLOUD_NAME'),
-    api_key=config('CLOUDINARY_API_KEY'),
-    api_secret=config('CLOUDINARY_API_SECRET')
-)
+# Cloudinary
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
